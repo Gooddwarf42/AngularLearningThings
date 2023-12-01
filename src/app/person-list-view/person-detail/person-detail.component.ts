b1fd948464dd4ecfa9f72265fb3ed9356bf8864d
Marco Girardi <marco.girardi@factorymind.com>
2023-12-01 15:45:09 +0100

    made details component

--- src/app/person-list-view/person-detail/person-detail.component.ts
+++ src/app/person-list-view/person-detail/person-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonService } from '../../person.service';
+import { ActivatedRoute } from '@angular/router';
+import { Person } from '../../person';
 
 @Component({
   selector: 'app-person-detail',
@@ -10,4 +13,13 @@ import { CommonModule } from '@angular/common';
 })
 export class PersonDetailComponent {
 
+  public person: Person;
+
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonService
+  ) {
+    const personId: number = Number(this.route.snapshot.params['id']);
+    this.person = personService.GetPersonById(personId);
+  }
 }
