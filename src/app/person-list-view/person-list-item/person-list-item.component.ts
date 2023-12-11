fb8745de10b714554ed8823c4d116d85fa8f5ea3
Marco Girardi <marco.girardi@factorymind.com>
2023-12-11 10:15:35 +0100

    making a pipe

--- src/app/person-list-view/person-list-item/person-list-item.component.ts
+++ src/app/person-list-view/person-list-item/person-list-item.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Person } from '../../person';
 import { Router } from '@angular/router';
+import { PhonePipe } from "../../phone.pipe";
 
 @Component({
-  selector: 'app-person-list-item',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './person-list-item.component.html',
-  styleUrl: './person-list-item.component.scss'
+    selector: 'app-person-list-item',
+    standalone: true,
+    templateUrl: './person-list-item.component.html',
+    styleUrl: './person-list-item.component.scss',
+    imports: [CommonModule, PhonePipe]
 })
 export class PersonListItemComponent {
   @Input() person!: Person;
