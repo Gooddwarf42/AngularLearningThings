8581ef85d4321045f9d0003519f15ce2ce7503a3
Marco Girardi <marco.girardi@factorymind.com>
2023-12-11 11:40:19 +0100

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +13,30 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 export class LoginComponent {
   constructor(
     private formBuilder: FormBuilder,
+    private router: Router,
   ) {
   }
 
   public loginFormGroup = this.formBuilder.group(
     {
-      username: this.formBuilder.control(''),
-      password: this.formBuilder.control(''),
+      username: this.formBuilder.control<string | null>(null),
+      password: this.formBuilder.control<string | null>(null),
     }
   )
 
   handleSubmit() {
-    // TODO
+    // ValidateLoginData
+    if (!ValidateForm(this.loginFormGroup)) {
+      alert("Login invalid");
+      return;
+    }
+
+    localStorage.setItem("loginmKey", "drago");
+    this.router.navigate(['/home/list']);
   }
 }
+function ValidateForm(loginFormGroup: FormGroup<{ username: FormControl<string | null>; password: FormControl<string | null>; }>) {
+  const mimmo = loginFormGroup.getRawValue();
+  return mimmo.password != null && mimmo.username == mimmo.password;
+}
+
